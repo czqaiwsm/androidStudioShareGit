971e29928f90587ee7d92f4a70b03bc935016fbe
czq <1148392049@qq.com>
2016-10-22 00:01:31 +0800

    弹出窗

--- shareLearn/src/main/java/com/share/learn/fragment/teacher/TeachCourseFragment.java
+++ shareLearn/src/main/java/com/share/learn/fragment/teacher/TeachCourseFragment.java
@@ -2,21 +2,27 @@ package com.share.learn.fragment.teacher;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import com.share.learn.R;
+import com.share.learn.activity.center.PCenterInfoUserActivity;
 import com.share.learn.activity.center.PurchaseCourseActivity;
 import com.share.learn.activity.login.LoginActivity;
 import com.share.learn.adapter.ContactAdpter;
 import com.share.learn.adapter.TeacherCourseAdapter;
 import com.share.learn.bean.CourseInfo;
 import com.share.learn.bean.TeacherDetailInfo;
+import com.share.learn.bean.UserInfo;
 import com.share.learn.bean.msg.Message;
 import com.share.learn.fragment.BaseFragment;
+import com.share.learn.fragment.center.PCenterInfoFragmentUser;
+import com.share.learn.utils.AlertDialogUtils;
 import com.share.learn.utils.BaseApplication;
 import com.share.learn.view.CustomListView;
+import com.ta.utdid2.android.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -72,6 +78,21 @@ public class TeachCourseFragment extends BaseFragment {
                     toClassActivity(TeachCourseFragment.this, LoginActivity.class.getName());
                     return;
                 }
+                UserInfo userInfo = BaseApplication.getUserInfo();
+                if(StringUtils.isEmpty(userInfo.getNickName()) ||
+                        StringUtils.isEmpty(userInfo.getGrade())|| StringUtils.isEmpty(userInfo.getAddress())){
+                    AlertDialogUtils.displayMyAlertChoice(getActivity(), "提示", "请完善个人资料及上课地址", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(mActivity,PCenterInfoUserActivity.class);
+                            startActivity(intent);
+                        }
+                    },null);
+
+                    return;
+                }
+
+
                 Intent intent = new Intent(mActivity,PurchaseCourseActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putSerializable("course",list.get(i-1));
